df2d12f30485ff083db50b1e6b01db3ff219a078
m1x0n <mmorozovm@gmail.com>
2015-05-19 22:55:56 +0300

    Added short array examples

---
+++ src/arrays.php
@@ -0,0 +1,20 @@
+<?php
+
+$oddNumbers = [1, 3, 5, 7];
+
+$frameworks = [
+    'js'   => 'Backbone',
+    'php'  => 'Zend',
+    'java' => 'Spring'
+];
+
+$randomStuff = [
+    0,
+    'Michael',
+    2 => 'Two',
+    new DateTime()
+];
+
+var_dump($oddNumbers);
+var_dump($frameworks);
+var_dump($randomStuff);
