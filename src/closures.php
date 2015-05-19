ccb63791a62917210caba1256f44827df2256c3d
m1x0n <mmorozovm@gmail.com>
2015-05-20 01:28:19 +0300

    Added closures

---
+++ src/closures.php
@@ -0,0 +1,41 @@
+<?php
+
+// Anonymous function
+function() {
+    return "Something special";
+};
+
+// Anonymous function assigned to a variable is a lambda
+$myBestFunction = function() {
+    return "Something special";
+};
+
+// Execution
+echo $myBestFunction() . PHP_EOL;
+
+// Pass to another function
+function testFunction($func)
+{
+    return $func() . " from test";
+}
+
+echo testFunction($myBestFunction) . PHP_EOL;
+
+// or even this way
+function handyFunction(Closure $func)
+{
+    return $func();
+}
+
+echo handyFunction(function() { return 123; }) . PHP_EOL;
+
+// Closures can use parameters from different scopes
+$number = 1;
+$leadingZeroes = function($numZeroes) use ($number) {
+    return str_pad($number, $numZeroes, '0', STR_PAD_LEFT);
+};
+
+echo $leadingZeroes(5) . PHP_EOL;
+
+var_dump($myBestFunction);
+var_dump($myBestFunction instanceof Closure);
