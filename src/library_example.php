e211783e5a1181e01af7a91d0ecd9f5c93065a8c
m1x0n <mmorozovm@gmail.com>
2015-05-23 01:56:08 +0300

    Added composer library autoloading examples

---
+++ src/library_example.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Run:
+ * php src/library_example.php -f http://blog.circleci.com/wp-content/uploads/2014/07/elephant.jpg -r 0.3 -w 0.5 -b
+ * */
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Parse options from command line
+$opts = array_merge([
+    'd' => 1,    // Dithering mode : 0 = DITHER_NONE, 1 = DITHER_ERROR
+    'f' => false,
+    'r' => 1.0,  // Resize factor 1.0 = 100%
+    'w' => 0.75, // Dither treshold weight
+], getopt("f:r:w:d:ib"));
+
+// An image file/url is required.
+$opts['f'] || die("Must specify an image file.\n");
+
+// The -i option inverts the image
+$image = Pixeler\Pixeler::image($opts['f'], $opts['r'], isset($opts['i']), $opts['w'], $opts['d']);
+
+// No colors if "-b" is passed
+isset($opts['b']) && $image->clearColors();
+
+// The Pixeler\Image instance render itself if casted to a string
+echo $image;
